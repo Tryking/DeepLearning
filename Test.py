82530f04cdb26d25ad7fd14f08248776cdd1d8e0
dengkaiting <dengkaiting@migu.cn>
2018-09-06 19:57:54 +0800

    update

--- Test.py
+++ Test.py
@@ -6,5 +6,5 @@ upstream = []
 output = reduce(lambda ret, conn: ret + conn.upstream_node.output * conn.weight, upstream, 0)
 print(output)
 
-s = lambda ret, conn: ret + conn.upstream_node.output * conn.weight
-print(s([], 0))
+s = lambda ret, conn: ret * conn
+print(s(2, 3))
--- basic/bp.py
+++ basic/bp.py
@@ -44,3 +44,69 @@ class Node(object):
         """
         output = reduce(lambda ret, conn: ret + conn.upstream_node.output * conn.weight, self.upstream, 0)
         self.output = sigmoid(output)
+
+    def calc_hidden_layer_delta(self):
+        """
+        节点属于隐藏层时，计算delta
+        """
+        downstream_delta = reduce(lambda ret, conn: ret + conn.downstream_node.delta * conn.weight, self.downstream, 0.0)
+        self.delta = self.output * (1 - self.output) * downstream_delta
+
+    def calc_output_layer_delta(self, label):
+        """
+        节点属于输出层时，计算delta
+        """
+        self.delta = self.output * (1 - self.output) * (label - self.output)
+
+    def __str__(self):
+        """
+        打印节点的信息
+        """
+        node_str = '%u-%u: output: %f delta: %f' % (self.layer_index, self.node_index, self.output, self.delta)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, '')
+        upstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.upstream, '')
+        return node_str + '\n\t downstream: ' + downstream_str + '\n\t upstream: ' + upstream_str
+
+
+class ConstNode(object):
+    def __init__(self, layer_index, node_index):
+        """
+        构造节点对象
+        """
+        self.layer_index = layer_index
+        self.node_index = node_index
+        self.downstream = []
+        self.output = 1
+
+    def append_downstream_connection(self, conn):
+        """
+        添加一个到下游节点的连接
+        """
+        self.downstream.append(conn)
+
+    def calc_hidden_layer_delta(self):
+        """
+        节点属于隐藏层时，计算delta
+        """
+        downstream_delta = reduce(lambda ret, conn: ret + conn.downstream_node.delta * conn.weight, self.downstream, 0.0)
+        self.delta = self.output * (1 - self.output) * downstream_delta
+
+    def __str__(self):
+        """
+        打印节点的信息
+        """
+        node_str = '%u-%u: output: 1' % (self.layer_index, self.node_index)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, '')
+        return node_str + '\n\tdownstream:' + downstream_str
+
+
+class Layer(object):
+    def __init__(self, layer_index, node_count):
+        """
+        初始化一层
+        """
+        self.layer_index = layer_index
+        self.nodes = []
+        for i in range(node_count):
+            self.nodes.append(Node(layer_index=layer_index, node_index=i))
+        self.nodes.append(ConstNode(layer_index=layer_index, node_index=node_count))
