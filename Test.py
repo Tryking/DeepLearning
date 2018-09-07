02a28e44036629f1ad78e5abdaa59c8ecdf7215c
dengkaiting <dengkaiting@migu.cn>
2018-09-07 16:31:33 +0800

    增加反向传播等实现

--- Test.py
+++ Test.py
@@ -2,9 +2,27 @@ from functools import reduce
 
 import requests
 
-upstream = []
-output = reduce(lambda ret, conn: ret + conn.upstream_node.output * conn.weight, upstream, 0)
-print(output)
+s = [1, 2, 3, 4, 5, 6, ]
+print(s[-3:-1])
+print(s[-3::-1])
+print(s[-3:-5:-1])
+print(s[-3::1])
+print(s[1:3])
+print(s[1::3])
 
-s = lambda ret, conn: ret * conn
-print(s(2, 3))
+s1 = s[:]
+print(s1)
+
+s = s[1:4]
+print(s1)
+print(s)
+
+mask = [0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80]
+
+
+def norm(number):
+    return map(lambda m: 0.9 if number & m else 0.1, mask)
+
+
+print(mask)
+print(list(norm(2)))
